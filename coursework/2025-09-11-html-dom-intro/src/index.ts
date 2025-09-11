c739214c104f8b972bd5eb57648e5aa083a73ec6
Kilian <kiliankager29@yahoo.com>
2025-09-11 16:41:40 +0200

    Refactor counter functionality to use increment factor from dropdown selection

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,18 +1,23 @@
 const counterbutton = document.getElementById("counterButton") as HTMLButtonElement;
 const counter = document.getElementById(`counter`) as HTMLParagraphElement;
 const counterdegrease = document.getElementById("count") as HTMLButtonElement;
+const Imgrement = document.getElementById('Imgrement') as HTMLInputElement;
+const select = document.getElementById('ImgrementSelect') as HTMLSelectElement;
 
 let clickcounter = 0;
+select.addEventListener('change', () => {
+    Imgrement.value = select.value
+})
 
 counterbutton.addEventListener('click', () => {
-    clickcounter++;
+    clickcounter += parseInt(Imgrement.value);
     counter.textContent = `${clickcounter}`
     if (clickcounter >= 0){
         counter.style.color = 'black';
     }
 })
 counterdegrease.addEventListener('click', () => {
-    clickcounter --;
+    clickcounter -= parseInt(Imgrement.value);
     counter.textContent = `${clickcounter}` 
     if (clickcounter < 0){
         counter.style.color = 'red';
