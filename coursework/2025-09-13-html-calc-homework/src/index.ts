eef2da1119bc4217f3bae99a50ea019f23ffeea4
Kilian <kiliankager29@yahoo.com>
2025-09-25 13:58:01 +0200

    Refactor HTML structure and improve styling for calculator interface; enhance TypeScript logic for calculation and error handling

--- coursework/2025-09-13-html-calc-homework/src/index.ts
+++ coursework/2025-09-13-html-calc-homework/src/index.ts
@@ -1,4 +1,26 @@
-const number1 = document.getElementById("num1") as HTMLHeadingElement;
+const result = document.getElementById('result') as HTMLParagraphElement;
+const calculate = document.getElementById('calculate') as HTMLButtonElement;
+const firstNum = document.getElementById('firstNum') as HTMLInputElement;
+const secondNum = document.getElementById('secondNum') as HTMLInputElement;
+const operation = document.getElementById('operation') as HTMLInputElement;
 
 
-
+calculate.addEventListener('click', () => {
+  if (firstNum.value !== '' && secondNum.value !== '') {
+    if (operation.value === '+') {
+      result.textContent = String(parseInt(firstNum.value) + parseInt(secondNum.value));
+    } else if (operation.value === '-') {
+      result.textContent = String(parseInt(firstNum.value) - parseInt(secondNum.value));
+    } else if (operation.value === '*') {
+      result.textContent = String(parseInt(firstNum.value) * parseInt(secondNum.value));
+    } else if (operation.value === '/') {
+      if (secondNum.value === '0') {
+        result.textContent = "You can't divide by Zero!";
+        return;
+      }
+      result.textContent = `${String(parseInt(firstNum.value) / parseInt(secondNum.value))}`;
+    }
+  }else{
+    result.textContent = "Please fill in the numbenrs!";
+  }
+});
