71a1914d6ab7ee32c4846b60206547c0311ead74
Kilian <kiliankager29@yahoo.com>
2025-09-25 15:35:52 +0200

    Replace static SVG elements with dynamic circle creation on button click; update event listener in TypeScript

--- coursework/20-svg/src/index.ts
+++ coursework/20-svg/src/index.ts
@@ -1,6 +1,13 @@
-const circle = document.getElementById("circle") as unknown as SVGCircleElement;
+const svg = document.getElementById("svg") as unknown as SVGCircleElement;
+const add = document.getElementById("add") as HTMLButtonElement;
 
-circle.addEventListener('click', () =>{
-    circle.setAttribute('fill', 'black');
+add.addEventListener('click', () =>{
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+    circle.setAttribute('fill', 'lime');
+    circle.setAttribute('stroke', 'green');
+    circle.setAttribute('cx',`${Math.random() * 200}`);
+    circle.setAttribute('cy',`${Math.random() * 200}`);
+    circle.setAttribute('r',`${Math.random() * 50}`);
 
+    svg.appendChild(circle);
 })
