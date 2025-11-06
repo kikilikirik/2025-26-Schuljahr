c05d705f6d772b1d3dd50e99efb5bce09b2e9d1b
Kilian <kiliankager29@yahoo.com>
2025-11-06 15:23:51 +0100

    Implement Connect Four game structure; add game logic and styles for the board and cells

--- coursework/connect-four-projekt/src/index.ts
+++ coursework/connect-four-projekt/src/index.ts
@@ -1,3 +1,37 @@
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+import './styles.css';
 
-helloWorldHeader.innerText += "!";
+console.log('Hello World');
+
+type Player = "red" | "yellow";
+type Cellstate = Player | "empty";
+
+class ConnectFourGame {
+    private boardElement: HTMLDivElement
+    private currentPlayer: Player = "red";
+
+    constructor() {
+        this.boardElement = document.getElementById("colored-rect") as HTMLDivElement;
+        this.createBoardCells();
+    }
+
+    private createBoardCells(): void {
+    // We create six rows and seven colums of white circles(divs)
+    for(let row= 0; row < 6; row++){
+        for(let colum = 0; colum < 7; colum++){
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            this.boardElement.appendChild(cell);
+        }
+    }
+    }
+
+    private switchPlayer(){
+        if(this.currentPlayer === "red"){
+            this.currentPlayer = "yellow"
+        }else{
+            this.currentPlayer = "red"
+        }
+    }
+}
+
+const game = new ConnectFourGame();
