003b47c1e2e993774e0c00b408655dae2013fb46
Kilian <kiliankager29@yahoo.com>
2025-12-29 11:57:04 +0100

    Add initial project structure for aquarium; include HTML, CSS, TypeScript files, and configuration, and implement fish classes with animations

---
+++ coursework/aquarium/src/fish.ts
@@ -0,0 +1,264 @@
+// Note: If you have something that only holds data, but does not have any methods,
+// use a `type` instead of a `class` in TypeScript.
+type Bounds = {
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+};
+
+// Check: Can you remember what an abstract class is?
+export abstract class Fish {
+  private readonly element: HTMLImageElement;
+  protected x: number;
+  protected y: number;
+  protected speedX: number;
+  protected speedY: number;
+  protected width: number = 0;
+  protected height: number = 0;
+  protected facingRight: boolean = true;
+
+  public loaded: boolean = false;
+
+  // Check: Can you remember what a constructor is?
+  constructor(protected aquarium: HTMLElement, src: string, startX: number, startY: number, speedX: number, speedY: number) {
+    this.x = startX;
+    this.y = startY;
+    this.speedX = speedX;
+    this.speedY = speedY;
+
+    // Create the element representing the fish
+    this.element = document.createElement('img');
+    this.element.src = src;
+    this.element.classList.add('fish');
+    this.element.style.position = 'absolute';
+
+    this.element.onload = () => {
+      // This is an important trick. We need to wait for the image
+      // to load before we can get its dimensions.
+      // 
+      // Read this article to learn more about naturalWidth and naturalHeight:
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/naturalWidth
+      this.width = this.element.naturalWidth * 0.33;
+      this.height = this.element.naturalHeight * 0.33;
+      this.element.width = this.width;
+      this.element.height = this.height;
+      this.loaded = true;
+      this.draw();
+    };
+
+    this.aquarium.appendChild(this.element);
+  }
+
+  // Check: Can you remember what an abstract method is?
+  abstract update(): void;
+
+  // Draw is not abstract, so extended classes can override it, 
+  // but they don't have to.
+  draw(): void {
+    if (!this.loaded) {
+      return;
+    }
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+
+    // Note that the BASE CLASS does the work of mirroring the image
+    // if the fish is moving in the wrong direction. This is correct because
+    // this is a behavior that ALL fish should have.
+    if (this.speedX < 0 && this.facingRight) {
+      // Fish is moving left, but the image is facing right, we must mirror it
+      this.element.style.transform = 'scaleX(-1)';
+      this.facingRight = false;
+    } else if (this.speedX > 0 && !this.facingRight) {
+      // Fish is moving right, but the image is facing left, we must mirror it
+      this.element.style.transform = 'scaleX(1)';
+      this.facingRight = true;
+    }
+  }
+
+  getBounds(): Bounds {
+    // Learn: This is how you create an instance of a type.
+    return {
+      left: this.x,
+      top: this.y,
+      right: this.x + this.width,
+      bottom: this.y + this.height,
+    };
+  }
+
+  // Called when the fish is detected to be out of bounds
+  handleOutOfBounds(_aquariumWidth: number, _aquariumHeight: number): void {
+    // Do nothing by default
+  }
+}
+
+// Check: Can you remember what `extends` means?
+export abstract class HorizontalCheckingFish extends Fish {
+  handleOutOfBounds(aquariumWidth: number, _aquariumHeight: number) {
+    const bounds = this.getBounds();
+    if (bounds.left < 0) {
+      // Fish is out of bounds on the left
+      this.x = 0;
+      this.speedX = -this.speedX;
+    } else if (bounds.right > aquariumWidth) {
+      // Fish is out of bounds on the right
+      this.x = aquariumWidth - this.width;
+      this.speedX = -this.speedX;
+    }
+  }
+}
+
+export abstract class BoundsCheckingFish extends HorizontalCheckingFish {
+  handleOutOfBounds(aquariumWidth: number, aquariumHeight: number) {
+    super.handleOutOfBounds(aquariumWidth, aquariumHeight);
+
+    const bounds = this.getBounds();
+    if (bounds.top < 0) {
+      this.y = 0;
+      this.speedY = -this.speedY;
+    } else if (bounds.bottom > aquariumHeight) {
+      this.y = aquariumHeight - this.height;
+      this.speedY = -this.speedY;
+    }
+  }
+}
+
+// HorizontalFish does not need vertical bounds checking,
+// so it extends HorizontalCheckingFish.
+export class HorizontalFish extends HorizontalCheckingFish {
+  update() {
+    // Move horizontally
+    this.x += this.speedX;
+  }
+}
+
+// CrissCrossFish extends BoundsCheckingFish,
+// so it has both horizontal and vertical bounds checking.
+export class CrissCrossFish extends BoundsCheckingFish {
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+}
+
+export class Staticfish extends Fish {
+    update(): void {
+        
+    }
+}
+
+export class RectangleFish extends Fish {
+  private step: number = 0;      // 0=rechts, 1=runter, 2=links, 3=hoch
+  private distance: number = 0;
+  private sideLength: number = 150;
+
+  update(): void {
+    const speed = Math.abs(this.speedX);
+
+    switch (this.step) {
+      case 0: // rechts
+        this.x += speed;
+        break;
+      case 1: // runter
+        this.y += speed;
+        break;
+      case 2: // links
+        this.x -= speed;
+        break;
+      case 3: // hoch
+        this.y -= speed;
+        break;
+    }
+
+    this.distance += speed;
+
+    if (this.distance >= this.sideLength) {
+      this.distance = 0;
+      this.step = (this.step + 1) % 4;
+    }
+  }
+}
+
+export class CircleFish extends Fish {
+  private angle: number = 0;
+  private centerX: number;
+  private centerY: number;
+  private radius: number = 80;
+
+  constructor(
+    aquarium: HTMLElement,
+    src: string,
+    startX: number,
+    startY: number,
+    speed: number
+  ) {
+    super(aquarium, src, startX, startY, speed, 0);
+    this.centerX = startX;
+    this.centerY = startY;
+  }
+
+  update(): void {
+    this.angle += 0.03;
+
+    this.x = this.centerX + Math.cos(this.angle) * this.radius;
+    this.y = this.centerY + Math.sin(this.angle) * this.radius;
+
+    // Richtung für Spiegelung
+    this.speedX = Math.cos(this.angle);
+  }
+}
+
+
+
+
+
+// TODO: Create at least two new fish classes
+// (e.g. a fish that swims in a circle, a fish that swims in a square, 
+// a fish that does not move at all, etc.)
+
+// SinusFish extends HorizontalCheckingFish, so it has horizontal bounds checking.
+export class SinusFish extends HorizontalCheckingFish {
+  private baseY: number;
+  private time: number = 0;
+
+  constructor(aquarium: HTMLElement, src: string, startX: number, startY: number, speedX: number) {
+    super(aquarium, src, startX, startY, speedX, 0);
+    this.baseY = startY;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.time += 0.05;
+    this.y = this.baseY + Math.sin(this.time) * 50; // Amplitude 50
+  }
+}
+
+export class FishManager {
+  // Note that we use the base class `Fish` here.
+  private fishes: Fish[] = [];
+
+  // Check: Can you remember what the `private` keyword does in 
+  // the parameter list of the constructor?
+  constructor(private aquarium: HTMLElement) {
+  }
+
+  addFish(fish: Fish) {
+    this.fishes.push(fish);
+  }
+
+  update() {
+    const width = this.aquarium.clientWidth;
+    const height = this.aquarium.clientHeight;
+
+    for (const fish of this.fishes) {
+      if (!fish.loaded) {
+        continue;
+      }
+
+      // Check: Can you remember what polymorphism is?
+      fish.update();
+      fish.handleOutOfBounds(width, height);
+      fish.draw();
+    }
+  }
+}
--- coursework/aquarium/src/index.ts
+++ coursework/aquarium/src/index.ts
@@ -1,4 +1,39 @@
 import './styles.css';
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+import { FishManager, HorizontalFish, CrissCrossFish, SinusFish, Staticfish, RectangleFish, CircleFish } from './fish';
 
-helloWorldHeader.innerText += "!";
+// Note that we can import images as strings. The result will be the path to the image.
+import fish1 from '../assets/fish1.png';
+import fish2 from '../assets/fish2.png';
+import fish3 from '../assets/fish3.png';
+import fish4 from '../assets/fish4.png';
+import fish5 from '../assets/fish5.png';
+import fish6 from '../assets/fish6.png';
+
+const aquarium = document.getElementById('aquarium')!;
+
+const manager = new FishManager(aquarium);
+
+// Add sample fish
+// Not moving Fish
+manager.addFish(new Staticfish(aquarium, fish1, 100, 100, 1.5, 0));
+// Rectangle moving Fish
+manager.addFish(new RectangleFish(aquarium, fish2, 200, 150, 2, 0));
+// Circle moving Fish
+manager.addFish(new CircleFish(aquarium, fish3, 300, 200, 1.5));
+
+// Random Fish
+//manager.addFish(new CrissCrossFish(aquarium, fish3, 200, 300, 1.5, 1.5));
+//manager.addFish(new CrissCrossFish(aquarium, fish4, 400, 400, -1.5, 2));
+
+// Sinus Fish
+//manager.addFish(new SinusFish(aquarium, fish5, 50, 150, 1.5));
+//manager.addFish(new SinusFish(aquarium, fish6, 600, 350, -1.5));
+
+function animate() {
+  manager.update();
+
+  // Check: Can you remember what `requestAnimationFrame` does?
+  requestAnimationFrame(animate);
+}
+
+animate();
